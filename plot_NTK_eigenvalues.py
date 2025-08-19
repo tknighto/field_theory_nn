957a5ba35fcf77f0f7c7117bb8480aeed8302351
Tiarnan McKnight <tiarnanmcknight@Tiarnans-MacBook-Air.local>
2025-08-19 21:39:27 +0100

    fixing wodths

--- plot_NTK_eigenvalues.py
+++ plot_NTK_eigenvalues.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as pl
 import torch
 
 data_dir = "loss_data"
-widths = range(5, 90, 20) # Use the same widths as in the training code
+widths = [1,5,25] # Use the same widths as in the training code
 
 plot_dir = "plots/eigenvalue_plots"
 os.makedirs(plot_dir, exist_ok=True)
@@ -124,7 +124,7 @@ import matplotlib.pyplot as pl
 
 
 data_dir = "loss_data"
-widths = range(5, 90, 20) # Use the same widths as in the training code
+widths = [1,5,25] # Use the same widths as in the training code
 
 plot_dir = "plots/ntk_change_plots"
 os.makedirs(plot_dir, exist_ok=True)
@@ -195,7 +195,7 @@ import matplotlib.pyplot as pl
 import torch
 
 data_dir = "loss_data"
-widths = range(5, 90, 20) # Use the same widths as in the training code
+widths = [1, 5, 25] # Use the same widths as in the training code
 
 plot_dir = "plots/loss_ntk_combined_plots"
 os.makedirs(plot_dir, exist_ok=True)
